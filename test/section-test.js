d407cf6202c2095e80341d6b25fd3c5c4e5cadfd
Craig Condon <craig.condon@hellosign.com>
2015-05-13 11:02:26 -0700

    add sections

--- test/section-test.js
+++ test/section-test.js
@@ -36,6 +36,26 @@ describe(__filename + "#", function() {
       section.appendChild(document.createTextNode("Hello World"));
       expect(section.render().toString()).to.be("<div>Hello World</div>");
     });
+
+    it("can be cloned", function() {
+      var el = document.createElement("div");
+      el.appendChild(document.createTextNode("Hello World"));
+      var section = createSection(document, el);
+      expect(section.clone().render().toString()).to.be("<div>Hello World</div>");
+    });
+
+    it("can be removed", function() {
+      var el = document.createElement("span");
+      el.appendChild(document.createTextNode("Hello World"));
+      var section = createSection(document, el);
+
+      var el2 = document.createElement("div");
+      el2.appendChild(section.render());
+      expect(el2.toString()).to.be("<div><span>Hello World</span></div>");
+      section.remove();
+      expect(el2.toString()).to.be("<div></div>");
+
+    });
   });
 
   describe("fragments", function() {
@@ -44,33 +64,67 @@ describe(__filename + "#", function() {
       frag.appendChild(document.createTextNode("a"));
       frag.appendChild(document.createTextNode("b"));
       var section = createSection(document, frag);
-      expect(section.node.nodeType).to.be(11);
+      expect(section.start.nodeType).to.be(3);
+      expect(section.end.nodeType).to.be(3);
     });
 
-    xit("can create a marker", function() {
-      var el = document.createElement("div");
-      var sectNode = document.createElement("div");
-      el.appendChild(sectNode);
-      var section = createSection(document, sectNode);
+    it("can create a marker", function() {
+
+      var frag = document.createDocumentFragment();
+      frag.appendChild(document.createTextNode("a"));
+      frag.appendChild(document.createTextNode("b"));
+
+      var section = createSection(document, frag);
       var marker = section.createMarker();
-      expect(marker.path.length).to.be(1);
+      expect(marker.startPath.length).to.be(1);
+      expect(marker.endPath.length).to.be(1);
     });
 
-    xit("can create a section from a marker", function() {
-      var el = document.createElement("div");
-      var sectNode = document.createElement("div");
-      el.appendChild(sectNode);
-      var section = createSection(document, sectNode);
+    it("can create a section from a marker", function() {
+
+      var frag = document.createDocumentFragment();
+      frag.appendChild(document.createTextNode("a"));
+      frag.appendChild(document.createTextNode("b"));
+
+      var section = createSection(document, frag);
+
+      var div = document.createElement("div");
+      div.appendChild(section.render());
       var marker = section.createMarker();
-      var section2 = marker.createSection(el);
-      expect(section2.node).to.be(sectNode);
+      var section = marker.createSection(div);
+
+      expect(section.remove().render().toString()).to.be("ab");
     });
 
-    xit("can append a node to the section", function() {
-      var el = document.createElement("div");
-      var section = createSection(document, el);
+    it("can append a node to the section", function() {
+      var section = createSection(document, document.createDocumentFragment());
       section.appendChild(document.createTextNode("Hello World"));
-      expect(section.render().toString()).to.be("<div>Hello World</div>");
+      expect(section.render().toString()).to.be("Hello World");
+    });
+
+    it("can be cloned", function() {
+      var frag = document.createDocumentFragment();
+      frag.appendChild(document.createTextNode("a"));
+      frag.appendChild(document.createTextNode("b"));
+
+      var section = createSection(document, frag);
+      expect(section.clone().render().toString()).to.be("ab");
+
+      var div = document.createElement("div");
+      div.appendChild(section.render());
+      expect(section.clone().render().toString()).to.be("ab");
+    });
+
+    it("can be removed", function() {
+      var frag = document.createDocumentFragment();
+      frag.appendChild(document.createTextNode("a"));
+      frag.appendChild(document.createTextNode("b"));
+      var section = createSection(document, frag);
+      var div = document.createElement("div");
+      div.appendChild(section.render());
+      expect(div.toString()).to.be("<div>ab</div>");
+      section.remove();
+      expect(div.toString()).to.be("<div></div>");
     });
   });
 });
