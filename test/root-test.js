5e16fd490598840493a9c7606cf7eecea9656070
Craig Condon <craig.condon@hellosign.com>
2015-05-20 14:39:03 -0700

    ability to ref components

--- test/root-test.js
+++ test/root-test.js
@@ -19,6 +19,28 @@ describe(__filename + "#", function() {
     var v = tpl.view({ a: "b", c: "d" });
 
     expect(v.render().toString()).to.be("<div>bd</div>");
+  });
 
+  it("can reference a component", function() {
+
+    function Component(section, vnode) {
+      this.node = section.document.createTextNode("");
+      section.appendChild(this.node);
+    }
+
+    Component.prototype.update = function() {
+      this.node.nodeValue = this.nodeValue;
+    }
+
+
+    var tpl = ivd.template(ivd.root(ivd.reference("ref1", ivd.element("component")), function(refs, context) {
+      refs.ref1.nodeValue = context.a;
+    }), {
+      components: {
+        component: Component
+      }
+    });
+
+    var v = tpl.view({ a: "b" });
   });
 });
